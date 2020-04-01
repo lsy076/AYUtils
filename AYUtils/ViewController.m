b92436e6c0e875ac8a119d2475721c3b89050cca
赖盛源 <756486059@qq.com>
2020-04-01 16:40:04 +0800

    no message

--- AYUtils/ViewController.m
+++ AYUtils/ViewController.m
@@ -10,10 +10,17 @@
 #import "AYUtils/AYWebView.h"
 
 @interface ViewController () <AYWebViewDelegate>
-
+@property (strong, nonatomic) AYWebView *webView;
 @end
 
 @implementation ViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.webView goBack];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,8 +31,8 @@
     [webView loadUrl:@"https://www.baidu.com"];
     webView.webViewDelegate = self;
     [self.view addSubview:webView];
+    self.webView = webView;
     
-    NSLog(@"%@", self.view);
 }
 
 - (void)webView:(AYWebView *)webView didFinishLoadedWithURL:(NSString *)url
